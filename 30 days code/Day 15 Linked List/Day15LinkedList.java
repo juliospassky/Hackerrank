24aa2801b2fc34b50574e59995f6a069cb2f4c75
JulioSpassky <juliocto2015@gmail.com>
2020-11-17 18:01:04 -0300

    :rocket: Day 15

---
+++ 30 days code/Day 15 Linked List/Day15LinkedList.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static  Node insert(Node head,int data) {
+        //Complete this method
+        if(Objects.isNull(head))
+            return new Node(data);
+        
+        Node n = head;
+        while (true){
+            if(n.next != null){
+                n = n.next;
+            }
+            else{
+                n.next = new Node(data);
+                break;
+            }
+        }
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
