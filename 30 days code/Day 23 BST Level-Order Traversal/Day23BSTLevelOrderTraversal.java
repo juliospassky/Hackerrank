f46ce0a9fdbde7073085954b260d3e8147ff6bb1
JulioSpassky <juliocto2015@gmail.com>
2020-11-28 16:14:32 -0300

    :rocket: Day 23

---
+++ 30 days code/Day 23 BST Level-Order Traversal/Day23BSTLevelOrderTraversal.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+static void levelOrder(Node root){
+      //Write your code here
+    Queue<Node> queue = new LinkedList();
+    queue.add(root);
+    
+    while(!queue.isEmpty()){
+        Node current = queue.remove();
+        System.out.print( current.data + " ");
+        if (current.left!=null) queue.add(current.left);
+        if (current.right!=null) queue.add(current.right);
+    }
+}
+
+public static Node insert(Node root,int data){
