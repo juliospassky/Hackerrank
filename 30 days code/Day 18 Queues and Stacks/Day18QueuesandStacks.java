d4462f72e9bbcf373b90762199150ffafb7c5a45
JulioSpassky <juliocto2015@gmail.com>
2020-11-28 16:22:16 -0300

    :rocket: Day 18 refactored

--- 30 days code/Day 18 Queues and Stacks/Day18QueuesandStacks.java
+++ 30 days code/Day 18 Queues and Stacks/Day18QueuesandStacks.java
@@ -1,60 +1,29 @@
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Solution {
     
-    private static String queue = "";
-    private static String stack = "";
+    private static Queue<Character> queue = new LinkedList<Character>();
+    private static Stack<Character> stack = new Stack<Character>();
     
     private static char popCharacter() {
-        char popedChar = stack.charAt(stack.length() - 1);
-        stack = stack.substring(0, stack.length()-1);
-        return popedChar;
+       return stack.pop();
     }
 
     private static char dequeueCharacter() {
-        char dequeuedChar = queue.charAt(0);
-        queue = queue.substring(1, queue.length());
-        return dequeuedChar;
+        return queue.remove();
     }
 
 
     private static void pushCharacter(char ch) {
-        queue += ch;
+        queue.add(ch);
     }
 
     private static void enqueueCharacter(char ch) {
-        stack += ch;
+        stack.push(ch);
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        scan.close();
-
-        // Convert input String to an array of characters:
-        char[] s = input.toCharArray();
-
-        // Create a Solution object:
-        Solution p = new Solution();
-
-        // Enqueue/Push all chars to their respective data structures:
-        for (char c : s) {
-            p.pushCharacter(c);
-            p.enqueueCharacter(c);
-        }
-
-        // Pop/Dequeue the chars at the head of both data structures and compare them:
-        boolean isPalindrome = true;
-        for (int i = 0; i < s.length/2; i++) {
-            if (p.popCharacter() != p.dequeueCharacter()) {
-                isPalindrome = false;                
-                break;
-            }
-        }
-
-        //Finally, print whether string s is palindrome or not.
-        System.out.println( "The word, " + input + ", is " 
-                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
-    }
-}
